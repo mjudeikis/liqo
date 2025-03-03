91090a68dd1768129f6ece703cb6c748346534fb
Mangirdas Judeikis <Mangirdas@Judeikis.LT>
2025-03-03 13:18:03 +0200

    CARRY: swap VN image

--- cmd/liqo-controller-manager/main.go
+++ cmd/liqo-controller-manager/main.go
@@ -164,7 +164,7 @@ func main() {
 	flag.Var(&loadBalancerClasses, "load-balancer-classes", "List of load balancer classes offered by the cluster. Example:\"metallb;default\"")
 
 	// Virtual-kubelet parameters
-	kubeletImage := flag.String("kubelet-image", "ghcr.io/liqotech/virtual-kubelet", "The image of the virtual kubelet to be deployed")
+	kubeletImage := flag.String("kubelet-image", "ghcr.io/mjudeikis/virtual-kubelet", "The image of the virtual kubelet to be deployed")
 	flag.Var(&kubeletExtraAnnotations, "kubelet-extra-annotations", "Extra annotations to add to the Virtual Kubelet Deployments and Pods")
 	flag.Var(&kubeletExtraLabels, "kubelet-extra-labels", "Extra labels to add to the Virtual Kubelet Deployments and Pods")
 	flag.Var(&kubeletExtraArgs, "kubelet-extra-args", "Extra arguments to add to the Virtual Kubelet Deployments and Pods")
